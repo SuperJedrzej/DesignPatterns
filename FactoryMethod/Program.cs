1b4418cf8fc5fc0f355e1f47ec116102f94c9d30
SuperJedrzej <j.kubiak164@gmail.com>
2018-11-16 22:15:39 +0100

    Abstract Factory

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var point = Point.Factory.NewPolarPoint(1.0, Math.PI / 2);
+            Console.WriteLine(point);
+        }
+    }
+}
